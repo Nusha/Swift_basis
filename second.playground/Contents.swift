140d12be638fd73c8d2ba0ee16919bcebd566a04
Ivan <i.balabai@gmail.com>
2023-06-04 22:17:15 +0300

    Task 2 finished

---
+++ second.playground/Contents.swift
@@ -0,0 +1,11 @@
+let a = 7
+let b = 20
+if a * b > 10 {
+    print(a)
+} else {
+    print(b)
+}
+// попробуем тернарный
+let res = a * b > 10 ? a : b
+print(res)
+// completed
---
+++ second.swift
@@ -0,0 +1,8 @@
+//
+//  second.swift
+//  
+//
+//  Created by Иван  Балабай  on 04.06.2023.
+//
+
+import Foundation
